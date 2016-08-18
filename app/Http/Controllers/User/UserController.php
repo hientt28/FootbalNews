91be1c968b3fd8769c47e592c3466e0c27520e8b
Trinh Hien <trinhthihien05091995@gmail.com>
2016-08-18 16:26:13 +0700

    init

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use  App\Repositories\BaseRepository;
+
+class UserController extends Controller
+{
+    private $userRepository;
+
+    public function __construct(BaseRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return redirect()->route('/');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+       //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+       //
+    }
+
+    public function update()
+    {
+       //
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,43 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+Route::auth();
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/', [
+        'as' => '/',
+        'uses' => 'HomeController@index',
+    ]);
+    Route::group(['prefix' => 'admin'], function () {
+        Route::get('/', [
+            'as' => 'admin.welcome',
+            'uses' => 'HomeController@welcome'
+        ]);
+    });
+    Route::group(['prefix' => 'users', 'namespace' => 'User'], function () {
+        Route::get('/', [
+            'as' => 'users.welcome',
+            'uses' => 'UserController@index'
+        ]);
+        Route::resource('news', 'NewController');
+        Route::resource('matches', 'MatchController', ['only' => ['index', 'show']]);
+    });
+
+    Route::group(['prefix' => 'login'], function () {
+        Route::get('social/{network}', [
+            'as' => 'loginSocialNetwork',
+            'uses' => 'SocialNetworkController@callback',
+        ]);
+        Route::get('{accountSocial}/redirect', [
+            'as' => 'redirectSocialNetwork',
+            'uses' => 'SocialNetworkController@redirect',
+        ]);
+    });
+});
